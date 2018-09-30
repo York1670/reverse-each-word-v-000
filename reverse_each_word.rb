ad45dbbd5c120bc4bcac5c4b9f96eb17b9addee0
Bobbi York <york1670@gmail.com>
2018-09-30 20:48:19 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   strings = sentence.split
   reverse = []
   strings.each do |string|
-    reverse << string.reverse 
+    reverse << string.reverse
   end
   puts reverse
 end
